5b754f2734821cabaf3ea57280bc529fc41b22ad
Janel diBiccari <janel.dibiccari@gmail.com>
2014-10-15 12:40:37 -0400

    change wsgi file

--- thsite/wsgi.py
+++ thsite/wsgi.py
@@ -13,7 +13,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thsite.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
