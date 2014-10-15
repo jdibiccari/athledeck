72038a1cf91ffb3189ea45b1f09a18ada4a7648c
Janel diBiccari <janel.dibiccari@gmail.com>
2014-10-15 16:11:04 -0400

    css files not loading and athlete show

--- athledeck/views.py
+++ athledeck/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from . models import Athlete
 
 def index(request):
   athletes = Athlete.objects.all()
   return render(request, 'athletes/index.html', {'athletes': athletes})
+
+def show(request, pk):
+  athlete = get_object_or_404(Athlete, pk=pk)
+  return render(request, 'athletes/show.html', {'athlete': athlete})
