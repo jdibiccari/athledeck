e008fd2999ed41ce35cb4e923539dd7e66e08bb4
Janel diBiccari <janel.dibiccari@gmail.com>
2014-10-16 17:41:09 -0400

    remove django-s3 direct upload stuff

--- athledeck/forms.py
+++ athledeck/forms.py
@@ -1,10 +1,8 @@
 from django import forms
-from s3direct.widgets import S3DirectWidget
 
 from .models import Athlete
 
 class AthleteForm(forms.ModelForm):
-    image = forms.URLField(widget=S3DirectWidget(dest='destination_key'))
     def __init__(self, *args, **kwargs):
         super(AthleteForm, self).__init__(*args, **kwargs)
         for field_name in self.fields:
@@ -13,4 +11,4 @@ class AthleteForm(forms.ModelForm):
 
     class Meta:
         model = Athlete
-        fields = ('first_name', 'last_name', 'sport', 'league', 'team', 'bio', 'image')
+        fields = ('first_name', 'last_name', 'sport', 'league', 'team', 'bio',)
