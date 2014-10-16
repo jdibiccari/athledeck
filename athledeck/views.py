bcdb0034211f579c4b802d773a60777c645090a5
Janel diBiccari <janel.dibiccari@gmail.com>
2014-10-16 18:04:32 -0400

    pagination

--- athledeck/views.py
+++ athledeck/views.py
@@ -3,9 +3,22 @@ from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404, render
 from .models import Athlete
 from .forms import AthleteForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
   athletes = Athlete.objects.order_by('last_name')
+  paginator = Paginator(athletes, 4)
+
+  page = request.GET.get('page')
+  try:
+      athletes = paginator.page(page)
+  except PageNotAnInteger:
+      # If page is not an integer, deliver first page.
+      athletes = paginator.page(1)
+  except EmptyPage:
+      # If page is out of range (e.g. 9999), deliver last page of results.
+      athletes = paginator.page(paginator.num_pages)
+
   return render(request, 'athletes/index.html', {'athletes': athletes})
 
 def show(request, pk):
