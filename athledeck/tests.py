0b24fa7d20b59be26afa7c60f0e8c9eb5525f80e
Janel diBiccari <janel.dibiccari@gmail.com>
2014-10-16 12:09:43 -0400

    background image

--- athledeck/tests.py
+++ athledeck/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 from .models import Athlete
+from django.core.urlresolvers import reverse
 
+
+
+class AthleteViewTests(TestCase):
+  def test_index_view_with_no_athletes(self):
+    
