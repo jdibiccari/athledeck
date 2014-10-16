464db5f1326a3b9f300e7342a9e12390555dfd30
Janel diBiccari <janel.dibiccari@gmail.com>
2014-10-16 02:21:53 -0400

    customized form

--- athledeck/forms.py
+++ athledeck/forms.py
@@ -3,6 +3,11 @@ from django import forms
 from .models import Athlete
 
 class AthleteForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AthleteForm, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+          field = self.fields.get(field_name) 
+          field.widget = forms.TextInput(attrs={'placeholder': field.label, 'class': 'form-control', 'label': ''})
 
     class Meta:
         model = Athlete
