9860862f9dba2fd8903791f9e9f3720bc01c9728
Janel diBiccari <janel.dibiccari@gmail.com>
2014-10-16 12:53:57 -0400

    change model, form, show to add bio

--- athledeck/models.py
+++ athledeck/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Athlete(models.Model):
   first_name = models.CharField(max_length = 140)
   last_name = models.CharField(max_length = 140)
+  bio = models.TextField(max_length = 400, default= "")
   sport = models.CharField(max_length = 140)
   league = models.CharField(max_length = 140)
   team = models.CharField(max_length = 140)
