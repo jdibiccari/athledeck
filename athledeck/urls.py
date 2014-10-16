13baf2fa4378e7722d034fb826044d9f083729a9
Janel diBiccari <janel.dibiccari@gmail.com>
2014-10-15 22:10:50 -0400

    new and edit form and templates

--- athledeck/urls.py
+++ athledeck/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.index),
     url(r'^athlete/(?P<pk>[0-9]+)/$', views.show),
-    url(r'^athlete/new/$', views.new),
+    url(r'^athlete/new/$', views.new, name='athlete_new'),
+    url(r'^athlete/(?P<pk>[0-9]+)/edit/$', views.edit, name='athlete_edit'),
 )
