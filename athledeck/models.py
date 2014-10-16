355de18e4a2051cff5136c92383f90a0901d9527
Janel diBiccari <janel.dibiccari@gmail.com>
2014-10-16 17:40:01 -0400

    default image, no upload option

--- athledeck/models.py
+++ athledeck/models.py
@@ -3,10 +3,11 @@ from django.db import models
 class Athlete(models.Model):
   first_name = models.CharField(max_length = 140)
   last_name = models.CharField(max_length = 140)
-  bio = models.TextField(max_length = 400, default= "")
+  bio = models.TextField(max_length = 600, default= "")
   sport = models.CharField(max_length = 140)
   league = models.CharField(max_length = 140)
   team = models.CharField(max_length = 140)
+  image = models.ImageField(upload_to='images/', default='/static/images/no-image.png')
 
   def __str__(self):
     return "%s %s" % (str(self.first_name), str(self.last_name))
--- static/js/athledeck.js
+++ static/js/athledeck.js
@@ -1,3 +1,4 @@
+//JQuery not available in this file--look into why
 $(document).ready(function() {
   $('ul.nav li').on('click', function(){
     this.addClass('active');
